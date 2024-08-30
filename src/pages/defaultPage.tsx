00d37741f509332e5dac36def3c9a62296f1bcbf
chacha <yushin.pak@gmail.com>
2024-08-30 20:44:11 +0900

    Feat: mobile asset 교체

--- src/pages/defaultPage.tsx
+++ src/pages/defaultPage.tsx
@@ -7,24 +7,29 @@ import red_fill_color from '../assets/sub/red_fill_color.jpg';
 
 // web asset
 import home from '../assets/main/pc_1.jpg';
-import introduce_caplus from '../assets/main/pc_2.jpg'; 
+import introduce_caplus from '../assets/main/pc_2.jpg';
 import introduce_detail from '../assets/main/pc_3.jpg';
 import curriculum from '../assets/main/pc_4.jpg';
 import application_guide from '../assets/main/pc_5.jpg';
 import ending from '../assets/main/pc_6.jpg';
 
 // mobile asset
-import content_mobile from '../assets/main/caplus_mobile.jpg';
+import home_mobile from '../assets/main/mobile_1.jpg';
+import introduce_caplus_mobile from '../assets/main/mobile_2.jpg';
+import introduce_detail_mobile from '../assets/main/mobile_3.jpg';
+import curriculum_mobile from '../assets/main/mobile_4.jpg';
+import application_guide_mobile from '../assets/main/mobile_5.jpg';
+import ending_mobile from '../assets/main/mobile_6.jpg';
 
 function DefaultPage() {
   return (
     <>
       <Container>
         <Header>
-          <Link href="#home-section">
+          <Link href="#header-space(home)">
             <TextLogo src={text_logo} alt="카플러스" />
+            <ImageLogo src={image_logo} alt="카플러스" />
           </Link>
-          <ImageLogo src={image_logo} alt="카플러스" />
           <ButtonContainer>
             <Link href="#introduce">
               <Button>소개</Button>
@@ -44,30 +49,58 @@ function DefaultPage() {
             </a>
           </ButtonContainer>
         </Header>
-        <HeaderSpace />
+        <HeaderSpace id="header-space(home)" />
         <ContentContainer>
-          <WebSection src={home} id="home-section" alt="홈 이미지" />
+          <WebSection src={home} alt="홈 이미지" />
           <WebSection src={introduce_caplus} alt="학회 소개" />
-          <WebSection id="introduce" src={introduce_detail} alt="디테일 학회 소개" />
+          <WebSection
+            id="introduce"
+            src={introduce_detail}
+            alt="디테일 학회 소개"
+          />
           <WebSection id="curriculum" src={curriculum} alt="커리큘럼" />
           <WebSection id="apply" src={application_guide} alt="지원안내" />
-          <WebSection src={ending} alt="엔딩" />
-          <MobileSection src={content_mobile} alt="photo" />
-          <ApplyContainer>
-            <ApplyLink
-              href={`../public/applicant-form.docx`}
-              download="카플러스_50기_지원서.docx"
-            >
-              <ApplyButton>지원서 다운받기</ApplyButton>
-            </ApplyLink>
-            <ApplyLink
-              href="http://pf.kakao.com/_tlnhn/chat"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <ApplyButton>지원하기</ApplyButton>
-            </ApplyLink>
-          </ApplyContainer>
+          <EndingContainer>
+            <WebSection src={ending} alt="엔딩" />
+            <ApplyButtonContainer>
+              <ApplyLink
+                href={`../public/applicant-form.docx`}
+                download="카플러스_50기_지원서.docx"
+              >
+                <ApplyButton>지원서 다운받기</ApplyButton>
+              </ApplyLink>
+              <ApplyLink
+                href="http://pf.kakao.com/_tlnhn/chat"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <ApplyButton>지원하기</ApplyButton>
+              </ApplyLink>
+            </ApplyButtonContainer>
+          </EndingContainer>
+          <MobileSection src={home_mobile} alt="홈 이미지" />
+          <MobileSection src={introduce_caplus_mobile} alt="학회 소개" />
+          <MobileSection src={introduce_detail_mobile} alt="디테일 학회 소개" />
+          <MobileSection src={curriculum_mobile} alt="커리큘럼" />
+          <MobileSection src={application_guide_mobile} alt="지원안내" />
+          <EndingContainer>
+            <MobileSection src={ending_mobile} alt="엔딩" />
+            <MobileApplyButtonContainer>
+              <MobileApplyLink
+                href={`../public/applicant-form.docx`}
+                download="카플러스_50기_지원서.docx"
+              >
+                <MobileApplyButton>지원서 다운받기</MobileApplyButton>
+              </MobileApplyLink>
+              <ApplyLink
+                href="http://pf.kakao.com/_tlnhn/chat"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <MobileApplyButton>지원하기</MobileApplyButton>
+              </ApplyLink>
+            </MobileApplyButtonContainer>
+          </EndingContainer>
         </ContentContainer>
       </Container>
     </>
@@ -75,6 +108,100 @@ function DefaultPage() {
 }
 
 export default DefaultPage;
+
+const EndingContainer = styled.div`
+  height: auto;
+  position: relative;
+`;
+
+const ApplyButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: absolute;
+  bottom: 35%;
+  left: 50%;
+  transform: translateX(-50%); /* 좌우 중앙 정렬 */
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const MobileApplyButtonContainer = styled.div`
+  display: none;
+
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: absolute;
+    bottom: 35%;
+    left: 50%;
+    transform: translateX(-50%); /* 좌우 중앙 정렬 */
+  }
+`;
+
+const ApplyLink = styled.a`
+  text-decoration: none;
+  pointer-events: auto; 
+`;
+
+const MobileApplyLink = styled.a`
+  text-decoration: none;
+  pointer-events: auto; 
+`;
+
+const ApplyButton = styled.button`
+  font-family: 'Pretendard-Regular';
+  background: url(${red_fill_color}) no-repeat center center;
+
+  background-size: cover;
+  border: none;
+  border-radius: 35px;
+  color: black;
+  color: #d9b2b5;
+  font-size: 24px;
+  width: 250px;
+
+  margin: 0.3rem;
+
+  cursor: pointer;
+  padding: 10px 20px;
+  position: relative;
+  pointer-events: auto;
+  text-align: center;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const MobileApplyButton = styled.button`
+  @media (max-width: 768px) {
+    display: flex; 
+    justify-content: center;
+    font-family: 'Pretendard-Regular';
+    background: url(${red_fill_color}) no-repeat center center;
+
+    background-size: cover;
+    border: none;
+    border-radius: 35px;
+    color: black;
+    color: #d9b2b5;
+    width: 250px;
+    font-size: 15px;
+    width: 150px; /* 버튼 너비 설정 */
+
+    margin: 0.3rem;
+
+    cursor: pointer;
+    padding: 10px 20px;
+    position: relative;
+    pointer-events: auto;
+    text-align: center;
+  }
+`;
 
 const WebSection = styled.img`
   display: none;
@@ -150,6 +277,8 @@ const ImageLogo = styled.img`
   transform: translateX(-50%); /* 중앙 정렬 */
   width: 30px;
 
+  pointer-events: auto;
+
   @media (max-width: 768px) {
     width: 25px; /* 모바일에서 로고 크기 조정 */
   }
@@ -203,36 +332,18 @@ const Button = styled.button`
 `;
 
 const Container = styled.div`
+  background-color: black;
   pointer-events: none;
   min-height: 100vh;
   width: 100%;
   display: flex;
   flex-direction: column;
+  height: auto;
 `;
 
 const ContentContainer = styled.div`
   position: relative;
   box-sizing: border-box;
-`;
-
-const ApplyContainer = styled.div`
-  position: absolute; /* 절대 위치 지정 */
-  bottom: 9%; /* 컨텐츠 하단에서 10% 떨어지게 설정 */
-  left: 50%;
-  transform: translateX(-50%); /* 좌우 중앙 정렬 */
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  @media (max-width: 768px) {
-    bottom: 6%;
-  }
-`;
-
-const ApplyLink = styled.a`
-  text-decoration: none; /* 링크 기본 스타일 제거 */
-  margin: 10px 0; /* 버튼 간격 설정 */
-  pointer-events: auto; /* 링크를 클릭 가능하게 설정 */
 `;
 
 const Link = styled.a`
@@ -242,28 +353,4 @@ const Link = styled.a`
   display: flex;
   align-items: center;
   justify-content: center;
-`;
-
-const ApplyButton = styled.button`
-  font-family: 'Pretendard-Regular';
-  background: url(${red_fill_color}) no-repeat center center; /* 배경 이미지 설정 */
-
-  background-size: cover; /* 이미지 크기를 버튼 크기에 맞게 조정 */
-  border: none;
-  border-radius: 35px;
-  color: black;
-  color: #d9b2b5;
-  font-size: 24px;
-  width: 250px;
-
-  cursor: pointer;
-  padding: 10px 20px;
-  position: relative;
-  pointer-events: auto; /* 버튼을 클릭 가능하게 설정 */
-  text-align: center; /* 버튼 텍스트 가운데 정렬 */
-
-  @media (max-width: 768px) {
-    font-size: 15px;
-    width: 150px; /* 버튼 너비 설정 */
-  }
 `;
